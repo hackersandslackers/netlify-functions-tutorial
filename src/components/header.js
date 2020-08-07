684191d9fbde14256b6dd9e939f22d9680595bbb
Todd Birchard <toddbirchard@gmail.com>
2020-08-06 20:21:22 -0400

    Styles.

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 900,
+        maxWidth: 800,
         padding: `1.45rem 1.0875rem`,
       }}
     >
--- src/components/layout.js
+++ src/components/layout.js
@@ -29,7 +29,7 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 900,
+          maxWidth: 800,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
